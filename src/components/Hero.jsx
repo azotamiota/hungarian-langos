cea2b296d5834b0657880998fab8f385ff1c1e88
azotamiota <azotamiota@gmail.com>
2022-07-12 17:45:42 +0100

    Design update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import cloudImage from '../images/cloud-removebg.png';
 
 export default function Hero() {
   return <>
     <div id="hero">
-      <header>Sale with background</header>
-      <p id="introduction"></p>
+      <img id='hero-image'src={cloudImage} alt=""/>
+      <header><span id='red-letters'>Hun</span><span id='white-letters'>gar</span><span id='green-letters'>ian</span> Langos</header>
+      <p id="introduction">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sunt ducimus minus consequuntur quis facere esse dignissimos.</p>
     </div>
   </>;
 }

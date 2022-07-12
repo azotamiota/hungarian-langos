9f1aff67c8f1f30cd09e38f9ed22f4973006cb3f
azotamiota <azotamiota@gmail.com>
2022-07-12 21:52:16 +0100

    Services description done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,7 @@ export default function Services() {
   return <>
   <div id='services'>
       <header id='services-header'><span className='red-letters'>Mi kap</span><span className='green-letters'>hato nalunk</span></header>
+      <p id='services-description'>Ide johet valami altalanos leiras az etelekrol, italokrol...pl, hogy foleg magyaros eteleket kinalunk, es lehet kapni meggy markat :) </p>
   </div>;
   </>
 }

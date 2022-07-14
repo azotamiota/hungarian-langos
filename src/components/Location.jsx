699254135ba68601d204dc7cd2d2bef29c0c768e
azotamiota <azotamiota@gmail.com>
2022-07-14 22:04:20 +0100

    add Location component

---
+++ src/components/Location.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Location() {
+  return <>
+    <div id="location">
+        <div id="location-text">
+            <header id='location-header'>Location</header>
+            <div>1a Nelson Parade<br /> Bedminster,<br /> Bristol<br /> BS3 4JA</div>
+        </div>
+        <iframe title='google-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2486.7091519030328!2d-2.594712384366621!3d51.44513507962494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48718d61e6941c07%3A0xe5b3471693506f82!2sHungarian%20Langos!5e0!3m2!1sen!2suk!4v1657829476599!5m2!1sen!2suk" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </>
+}
+
+// "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin"

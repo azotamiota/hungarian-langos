67783922e59772407420472216a2dd8b75296497
azotamiota <azotamiota@gmail.com>
2022-07-14 23:48:09 +0100

    Create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return <>
+    <footer>
+      {/* <header id='footer-header'>Contact</header> */}
+        <div id="footer-icons">
+            <div className="contact-logo"><i className="c-icon fa-lg fa-solid fa-phone"></i>07234023402</div>
+            <div className="contact-logo"><i className="c-icon fa-lg fa-solid fa-envelope"></i>valami@valami.com</div>
+            <div id="brand-logos">
+              <div className="brand-logo">
+                <i className="fa-lg fa-brands fa-facebook"></i>
+              </div>
+              <div className="brand-logo">
+                <i className="fa-lg fa-brands fa-instagram"></i>
+              </div>
+              <div className="brand-logo">
+                <i className="fa-lg fa-brands fa-twitter"></i>
+              </div>
+            </div>
+        </div>
+    </footer>
+  </>
 }

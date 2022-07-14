9c6a56ad41b430e3b1919097caa401240758dc56
azotamiota <azotamiota@gmail.com>
2022-07-14 22:05:26 +0100

    add Location component to App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import './App.css';
 import React from "react";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Portfolio from "./components/Portfolio";
+// import Portfolio from "./components/Portfolio";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import ScrollToTop from "./components/ScrollToTop";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 import FoodGroups from './components/FoodGroups';
 import Separator from './components/Separator';
+import Location from './components/Location';
 
 export default function App() {
   return (
   <>
-  <ScrollToTop />
-  <Navbar />
-  <Hero />
-  <Separator />
-  <FoodGroups />
-  <Separator />
-  <Products />
-  <Portfolio />
-  <Testimonials />
-  <Footer />
+    {/* <ScrollToTop /> */}
+    <Navbar />
+    <Hero />
+    <Separator />
+    <FoodGroups />
+    <Separator />
+    <Products />
+    <Location />
+    {/* <Portfolio /> */}
+    {/* <Testimonials /> */}
+    <Footer />
   </>
   )
 }

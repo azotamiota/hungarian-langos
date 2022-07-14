78442908159c6f61ad8a0350e0221d5fa7bb29f6
azotamiota <azotamiota@gmail.com>
2022-07-14 17:32:35 +0100

    update products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,47 @@
 import React from "react";
 
 export default function Products() {
-  return <div>Products</div>;
+  return <>
+  <div id='street-food' className='products'>
+    <header className='services-header'><span className='red-letters'>Street</span><span className='green-letters'>food</span></header>
+    <div className="cards-class">
+        <div className="food-card">
+          <div className="food-photo"></div>
+          <div className="food-card-description">Sajtos-tejfolos-zoldseges langos</div>
+          <div className="price">£3.45</div>
+        </div>
+        <div className="food-card">
+          <div className="food-photo"></div>
+          <div className="food-card-description">Sajtos-tejfolos-zoldseges langos</div>
+          <div className="price">£3.45</div>
+        </div>
+        <div className="food-card">
+          <div className="food-photo"></div>
+          <div className="food-card-description">Sajtos-tejfolos-zoldseges langos</div>
+          <div className="price">£3.45</div>
+        </div>
+    </div>
+  </div>;
+
+  <div id='bowls' className='products'>
+    <header className='services-header'><span className='red-letters'>Egytal</span><span className='green-letters'>etelek</span></header>
+    <div className="cards-class">
+        <div className="food-card">
+          <div className="food-photo2"></div>
+          <div className="food-card-description">Hagyomanyos magyar gulyas</div>
+          <div className="price">£4.15</div>
+        </div>
+        <div className="food-card">
+          <div className="food-photo2"></div>
+          <div className="food-card-description">Hagyomanyos magyar gulyas</div>
+          <div className="price">£4.15</div>
+        </div>
+        <div className="food-card">
+          <div className="food-photo2"></div>
+          <div className="food-card-description">Hagyomanyos magyar gulyas</div>
+          <div className="price">£4.15</div>
+        </div>
+    </div>
+  </div>;
+  </>
 }

c5604f699f01547ce49ee879d2fe713bc7b25084
azotamiota <azotamiota@gmail.com>
2022-07-14 19:16:04 +0100

    activate scroll to top button

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function ScrollToTop() {
-  return <div>Scrolltotop</div>;
+
+  const toTopButton = document.getElementById('to-top')
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      toTopButton.style.display = "flex";
+    } else {
+      toTopButton.style.display = "none";
+    }
+  }
+
+  window.onscroll = () => scrollFunction();
+  toTopButton.addEventListener('click', () => {
+    console.log('scroll up button clicked')
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  })
+  
+  return <>
+  <div id='to-top'><i class="fa-solid fa-arrow-up fa-lg"></i></div>
+  </>
 }

6e48052156292d88bfdb198d973f33c8c6343129
azotamiota <azotamiota@gmail.com>
2022-07-16 14:09:41 +0100

    change hero design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-// import logo from '../images/H-langos.png'
+// import logo from '../images/favicon.ico'
 
 
 export default function Hero() {
   return <>
     <div id="hero">
-      {/* <img id='hero-image'src={cloudImage} alt=""/> */}
         <div id="hero-header">
           <header style={{"text-align": "end","font-size": "30px"}}><u>Best</u> Streetfood<br/>in <strong>Bristol</strong><hr/></header>
-        {/* <img id='hero-logo' className="logo" src={logo} alt=""/> */}
-
         </div>
-      {/* <header><span className='red-letters'>Hun</span><span className='white-letters'>gar</span><span className='green-letters'>ian</span> Langos</header> */}
+        {/* <img id='letter-icon' src={logo} alt=""/> */}
       <div id="introduction">Ide johet valami bevezeto szoveg, valami frappans leiras a helyrol, mirol hires, mi a legfinomabb...stb.</div>
     </div>
   </>;

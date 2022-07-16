3447e7a54486ad8ee514fc2973829d0b67e5df3c
azotamiota <azotamiota@gmail.com>
2022-07-16 20:43:12 +0100

    clean the code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import React from "react";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-// import Portfolio from "./components/Portfolio";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
-// import ScrollToTop from "./components/ScrollToTop";
 // import Testimonials from "./components/Testimonials";
 import FoodGroups from './components/FoodGroups';
 import Separator from './components/Separator';
@@ -14,7 +12,6 @@ import Location from './components/Location';
 export default function App() {
   return (
   <>
-    {/* <ScrollToTop /> */}
     <Navbar />
     <Hero />
     <Separator />
@@ -22,7 +19,6 @@ export default function App() {
     <Separator />
     <Products />
     <Location />
-    {/* <Portfolio /> */}
     {/* <Testimonials /> */}
     <Footer />
   </>

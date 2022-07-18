939862cf4f33af9752a407da807f55edde43f6a8
azotamiota <azotamiota@gmail.com>
2022-07-18 17:07:09 +0100

    link instagram to logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Footer() {
   return <>
     <footer>
-      {/* <header id='footer-header'>Contact</header> */}
         <div id="footer-icons">
             <div className="contact-logo"><i className="c-icon fa-lg fa-solid fa-phone"></i>07234023402</div>
             <div className="contact-logo"><i className="c-icon fa-lg fa-solid fa-envelope"></i>valami@valami.com</div>
@@ -12,7 +11,7 @@ export default function Footer() {
                 <a href='https://www.facebook.com/paradeburger' target='blank'><i className="fa-lg fa-brands fa-facebook"></i></a>
               </div>
               <div className="brand-logo">
-                <i className="fa-lg fa-brands fa-instagram"></i>
+                <a href='https://www.instagram.com/hungarian_langos_bs3/' target='blank'><i className="fa-lg fa-brands fa-instagram"></i></a>
               </div>
               <div className="brand-logo">
                 <i className="fa-lg fa-brands fa-twitter"></i>

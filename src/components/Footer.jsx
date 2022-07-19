0ef5d4dff4163a48fc7d7aef1ddeb872888e002e
azotamiota <azotamiota@gmail.com>
2022-07-19 20:52:06 +0100

    add opening hours

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import Separator from './Separator';
+
 
 export default function Footer() {
   return <>
     <footer>
+      <header>Opening hours</header>
+        <div id="opening-hours">
+          <div>Monday</div>
+          <div>Closed</div>
+          <div>Tuesday</div>
+          <div>9:00 - 13:00</div>
+          <div>Wednesday</div>
+          <div>9:00 - 13:00</div>
+          <div>Thursday</div>
+          <div>9:00 - 13:00</div>
+          <div>Friday</div>
+          <div>9:00 - 14:00</div>
+          <div>Saturday</div>
+          <div>9:00 - 15:00</div>
+          <div>Sunday</div>
+          <div>Closed</div>
+        </div>
+        <Separator />
         <div id="footer-icons">
             <div className="contact-logo"><i className="c-icon fa-lg fa-solid fa-phone"></i>07234023402</div>
             <div className="contact-logo"><i className="c-icon fa-lg fa-solid fa-envelope"></i>valami@valami.com</div>

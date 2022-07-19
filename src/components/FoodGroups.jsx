48313937ffc265fcfc98e4c5356abae5affb9412
azotamiota <azotamiota@gmail.com>
2022-07-19 23:07:22 +0100

    update Hero & Food Group on tablet

--- src/components/FoodGroups.jsx
+++ src/components/FoodGroups.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
+
 
 export default function FoodGroups() {
+
+  const isBiggerThanMobile = useMediaQuery({
+  query: '(min-width: 425px)'
+  })
+
   return <>
   <div id='food-groups' className='products'>
       <header className='services-header'><span className='red-letters'>Mi kap</span><span className='green-letters'>hato nalunk</span></header>
       <p id='services-description'>Ide johet valami altalanos leiras az etelekrol, italokrol...pl, hogy foleg magyaros eteleket kinalunk, es lehet kapni meggy markat :) </p>
-      <div className="cards-class">
-        <div className="card">
+      <div className={(!isBiggerThanMobile && "cards-class") || 'cards-class-tablet'}>
+        <div className={(!isBiggerThanMobile && "card") || "card-tablet"}>
           <div className="services-logo"><i className="fa-lg fa-solid fa-utensils"></i></div>
-          <div className="card-description">Ide johet a szoveg az elerheto gyorsetelerol, pl a langosrol nehany mondatban</div>
+          <div className={(!isBiggerThanMobile && "card-description") || "card-description-tablet"}>Ide johet a szoveg az elerheto gyorsetelerol, pl a langosrol nehany mondatban</div>
           <div className="button"><a href='#street-food'>Mutasd</a></div>
         </div>
-        <div className="card">
+        <div className={(!isBiggerThanMobile && "card") || "card-tablet"}>
           <div className="services-logo" style={{color: 'green'}}><i className="fa-lg fa-solid fa-bowl-food"></i></div>
-          <div className="card-description">Ide mondjuk johet a szoveg az egytaletelekrol, pl szekely kaposzta es tarsai</div>
+          <div className={(!isBiggerThanMobile && "card-description") || "card-description-tablet"}>Ide mondjuk johet a szoveg az egytaletelekrol, pl szekely kaposzta es tarsai</div>
           <div className="button"><a href='#bowls'>Mutasd</a></div>
         </div>
-        <div className="card">
+        <div className={(!isBiggerThanMobile && "card") || "card-tablet"}>
           <div className="services-logo"><i className="fa-lg fa-solid fa-champagne-glasses"></i></div>
-          <div className="card-description">Ide pedig az elerheto hideg es forro italokrol johet a szoveg</div>
+          <div className={(!isBiggerThanMobile && "card-description") || "card-description-tablet"}>Ide pedig az elerheto hideg es forro italokrol johet a szoveg</div>
           <div className="button"><a href='#drinks'>Mutasd</a></div>
         </div>
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-// import logo from '../images/favicon.ico'
+import { useMediaQuery } from "react-responsive";
 
 
 export default function Hero() {
+
+  const isBiggerThanMobile = useMediaQuery({
+  query: '(min-width: 425px)'
+  })
+
   return <>
     <div id="hero">
         <div id="hero-header">
           <header style={{"text-align": "end","font-size": "30px"}}><u>Best</u> Streetfood<br/>in <strong>Bristol</strong><hr/></header>
         </div>
         {/* <img id='letter-icon' src={logo} alt=""/> */}
-      <div id="introduction">Ide johet valami bevezeto szoveg, valami frappans leiras a helyrol, mirol hires, mi a legfinomabb...stb.</div>
+      <div id={(!isBiggerThanMobile && "introduction") || "introduction-tablet"}>Ide johet valami bevezeto szoveg, valami frappans leiras a helyrol, mirol hires, mi a legfinomabb...stb.</div>
     </div>
   </>;
 }

3c45df2a5edd516029bfa166c8436919d9c79370
azotamiota <azotamiota@gmail.com>
2022-07-21 22:32:41 +0100

    .slidable cards

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import Separator from './Separator';
 
@@ -8,26 +8,28 @@ export default function Products() {
   const isBiggerThanMobile = useMediaQuery({
   query: '(min-width: 425px)'
   })
+  
+  const [position, setPosition] = useState(0)
 
   return <>
   <div id={isBiggerThanMobile && "street-food"} className='products'>
     <div className={(isBiggerThanMobile && "nav-arrow") || "hidden"} id="left-arrow">&#10094;</div>
     <div className={(isBiggerThanMobile && "nav-arrow") || "hidden"} id="right-arrow">&#10095;</div>
     <header><span className='red-letters'>Street</span><span className='green-letters'> food</span></header>
-    <div className="cards-class">
-        <div className="food-card first-group-card">
-          <div className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></div>
+    <div className={(isBiggerThanMobile && "slidable-cards cards-class") || "cards-class"} style={{left: position + 'px'}}>
+        <div className={(isBiggerThanMobile && "food-card-tablet") || "food-card first-group-card"}>
+          <div id='str-food-1' className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></div>
           <div className="food-card-description">Sajtos-tejfolos-zoldseges langos</div>
           <div className="price">£6.45</div>
         </div>
-        <div className={(isBiggerThanMobile && "hidden") || "food-card"}>
-          <div className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></div>
-          <div className="food-card-description">Sajtos-tejfolos-zoldseges langos</div>
+        <div className={(isBiggerThanMobile && "food-card-tablet") || "food-card"}>
+          <div id='str-food-2'className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></div>
+          <div className="food-card-description">Hot-Dog</div>
           <div className="price">£6.45</div>
         </div>
-        <div className={(isBiggerThanMobile && "hidden") || "food-card last-group-card"}>
-          <div className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></div>
-          <div className="food-card-description">Sajtos-tejfolos-zoldseges langos</div>
+        <div className={(isBiggerThanMobile && "food-card-tablet") || "food-card last-group-card"}>
+          <div id='str-food-3' className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></div>
+          <div className="food-card-description">Hamburger</div>
           <div className="price">£6.45</div>
         </div>
     </div>
@@ -37,17 +39,17 @@ export default function Products() {
     <header><span className='red-letters'>Egytal</span><span className='green-letters'> etelek</span></header>
     <div className="cards-class">
         <div className="food-card first-group-card">
-          <div className="food-photo2"></div>
+          <div id='str-food-3' className="food-photo"></div>
           <div className="food-card-description">Hagyomanyos magyar gulyas</div>
           <div className="price">£7.15</div>
         </div>
         <div className="food-card">
-          <div className="food-photo2"></div>
+          <div id='str-food-3' className="food-photo"></div>
           <div className="food-card-description">Hagyomanyos magyar gulyas</div>
           <div className="price">£7.15</div>
         </div>
         <div className="food-card last-group-card">
-          <div className="food-photo2"></div>
+          <div id='str-food-3' className="food-photo"></div>
           <div className="food-card-description">Hagyomanyos magyar gulyas</div>
           <div className="price">£7.15</div>
         </div>

8490219500ce9f93d30576d94baed347ed321fb3
azotamiota <azotamiota@gmail.com>
2022-07-30 21:15:41 +0100

    food group slide

--- src/components/FoodGroups.jsx
+++ src/components/FoodGroups.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import  { React, useEffect, useState } from "react";
+
 import { useMediaQuery } from "react-responsive";
 
 
@@ -8,22 +9,79 @@ export default function FoodGroups() {
   query: '(min-width: 425px)'
   })
 
+  const [position, setPosition] = useState(0)
+  const [widthDiff, setWidthDiff] = useState(0)
+  
+   if (position > 0) {
+        setPosition(0)
+      }
+
+    // let widthDiffs;
+    // if (slidableWidth > 0) {
+    //   if (Math.abs(position) > window.innerWidth - slidableWidth) {
+    //   setPosition(window.innerWidth - window.innerWidth - slidableWidth)
+    //   }
+    // }   
+
+  useEffect(() => {
+    
+    document.querySelector('.cards-class-tablet').style.left = `${position}px`;
+    // if (document.querySelector('.food-card-tablet')) {
+    //     console.log('if food card tablet class exist: ',Boolean(document.querySelector('.food-card-tablet')));
+    //   } else {
+    //     console.log(0)
+    //   }
+  }, [position]) 
+
   return <>
   <div id='food-groups' className='products'>
+      <div className={(isBiggerThanMobile && "nav-arrow") || "hidden"} id="left-arrow-food-group" onClick={() => {
+      if (position < 0) {
+         setPosition(position + window.innerWidth / 2)
+        } else {
+          setPosition(0)
+        }
+    }}>&#10094;</div>
+    <div className={(isBiggerThanMobile && "nav-arrow") || "hidden"} id="right-arrow-food-group" onClick={() => {
+      setWidthDiff(document.querySelector('.group-card-tablet').offsetWidth * 3 - window.innerWidth) //widthDiff will be  +200
+      if (position === 0) {
+        if (document.querySelector('.group-card-tablet').offsetWidth * 3 - window.innerWidth < window.innerWidth / 2) {
+          setPosition(0 - widthDiff);
+          document.querySelector('.card-class-tablet').style.left = `${window.innerWidth - document.querySelector('.group-card-tablet').offsetWidth * 3}px`;
+          console.log('position should be negative number here: ', position);
+            return     //position will be -400
+        } else {
+          setPosition(position - window.innerWidth / 2)
+        }
+      } else
+      if (Math.abs(position) + window.innerWidth / 2 > widthDiff) {
+        setPosition(0 - widthDiff)     //position will be -400
+      } else {
+        setPosition(position - window.innerWidth / 2)     //position will be -400
+      }
+      
+      // if (Math.abs(position) > widthDiff)  {
+      //   setPosition(0 - widthDiff)
+      // }
+
+       
+        console.log('position after: ', position, 'barlength - windowlength: ', widthDiff)
+      
+    }}>&#10095;</div>
       <header className='services-header'><span className='red-letters'>Me</span><span className='green-letters'>nu</span></header>
       <p id='services-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in ligula odio. Etiam porttitor neque eget maximus blandit. Vestibulum id ante sit amet eros elementum ullamcorper. </p>
       <div className={(!isBiggerThanMobile && "cards-class") || 'cards-class-tablet'}>
-        <div className={(!isBiggerThanMobile && "card") || "card-tablet"}>
+        <div className={(!isBiggerThanMobile && "card") || "group-card-tablet"}>
           <div className="services-logo"><i className="fa-lg fa-solid fa-utensils"></i></div>
           <div className={(!isBiggerThanMobile && "card-description") || "card-description-tablet"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in ligula odio. Etiam porttitor neque eget maximus blandit. Vestibulum id ante sit amet eros elementum ullamcorper.</div>
           <div className="button"><a href='#street-food'>Show</a></div>
         </div>
-        <div className={(!isBiggerThanMobile && "card") || "card-tablet"}>
+        <div className={(!isBiggerThanMobile && "card") || "group-card-tablet"}>
           <div className="services-logo" style={{color: 'green'}}><i className="fa-lg fa-solid fa-bowl-food"></i></div>
           <div className={(!isBiggerThanMobile && "card-description") || "card-description-tablet"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in ligula odio. Etiam porttitor neque eget maximus blandit. Vestibulum id ante sit amet eros elementum ullamcorper.</div>
           <div className="button"><a href='#bowls'>Show</a></div>
         </div>
-        <div className={(!isBiggerThanMobile && "card") || "card-tablet"}>
+        <div className={(!isBiggerThanMobile && "card") || "group-card-tablet"}>
           <div className="services-logo"><i className="fa-lg fa-solid fa-champagne-glasses"></i></div>
           <div className={(!isBiggerThanMobile && "card-description") || "card-description-tablet"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in ligula odio. Etiam porttitor neque eget maximus blandit. Vestibulum id ante sit amet eros elementum ullamcorper.</div>
           <div className="button"><a href='#drinks'>Show</a></div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { React, useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import Separator from './Separator';
 

52881330a25e48a8ddb2cdbf2a6d3b889759fbec
agiskorska <skorskaga@gmail.com>
2022-10-20 12:30:11 +0100

    Add dynamic title colouring

--- src/components/common/FoodSection/index.jsx
+++ src/components/common/FoodSection/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Product } from '..'
 import { Separator } from '../'
-function index({isBiggerThanMobile, products}) {
+function index({isBiggerThanMobile, products, name}) {
+  const splitInHalf = (text) => {
+    const twoHalves = []
+    const total = text.length
+    twoHalves.push(text.substring(0, Math.round(total/2)))
+    twoHalves.push(text.substring(Math.round(total/2), total))
+    return twoHalves
+  }
+
   return (
     <div id={(isBiggerThanMobile && "street-food") || 'false'} className='products'>
-      <header><span className='red-letters'>Starters &</span><span className='green-letters'> Appetisers</span></header>
-        {/* // style={{"right": `${position}`}} */}
+      <header><span className='red-letters'>{splitInHalf(name)[0]}</span><span className='green-letters'>{splitInHalf(name)[1]}</span></header>
       <div id="moveable-street-food" className={(isBiggerThanMobile && "slidable-cards") ||"cards-class"}> 
         {products.map((prod, index) => (<Product key={index} isBiggerThanMobile={isBiggerThanMobile} name={prod.name} price={prod.price} img={prod.image}/>))}
           
--- src/components/common/Product/index.jsx
+++ src/components/common/Product/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 function index({isBiggerThanMobile, name, price, img}) {
   return (
     <div className={(isBiggerThanMobile && "food-card-tablet") || "food-card first-group-card"}>
-      <img src={img} className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></img>
+      <img src={img} alt={name} className={(isBiggerThanMobile && "food-photo-tablet") ||"food-photo"}></img>
       <div className="food-card-description">{name}</div>
       <div className="price">{price}</div>
     </div>

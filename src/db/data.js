99a23b32c3b5ec389426cae969083546053c03a3
agiskorska <skorskaga@gmail.com>
2022-10-20 12:14:22 +0100

    Add temporary database

---
+++ src/db/data.js
@@ -0,0 +1,31 @@
+export const food = [
+  {
+    category: 'Starters & Appetisers',
+    products: [
+      {name: 'sample food', price: 6.45, image: 'https://images.unsplash.com/photo-1610137312377-f2334bc3b279?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'},
+      {name: 'mini hot-dog', price: 8.45, image: 'https://images.unsplash.com/photo-1613482084286-41f25b486fa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'},
+      {name: 'noodle soup', price: 5.45, image: 'https://images.unsplash.com/photo-1612927601601-6638404737ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'},
+      {name: 'chicken noodles', price: 9.45, image: 'https://images.unsplash.com/photo-1534939561126-855b8675edd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'}
+    ]
+  },
+  {
+    category: 'Mains',
+    products: [
+      {name: 'sample food', price: 6.45, image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80'},
+      {name: 'mini hot-dog', price: 8.45, image: 'https://images.unsplash.com/photo-1603133872878-684f208fb84b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80'},
+      {name: 'noodle soup', price: 5.45, image: ''},
+      {name: 'chicken noodles', price: 9.45, image: ''}
+    ]
+  },
+  {
+    category: 'Drinks',
+    products: [
+      {name: 'soft drinks', price: 6.45, image: 'https://images.unsplash.com/photo-1556881286-fc6915169721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'},
+      {name: 'hot drinks', price: 8.45, image: 'https://images.unsplash.com/photo-1535958636474-b021ee887b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'},
+      {name: 'beer', price: 5.45, image: 'https://images.unsplash.com/photo-1610137312377-f2334bc3b279?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'},
+      {name: 'alcohol drinks', price: 9.45, image: ''}
+    ]
+  },
+
+
+]

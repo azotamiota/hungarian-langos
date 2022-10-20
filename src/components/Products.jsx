7e6ab0e1b33b4584f02694f99876bda7e45ecbba
agiskorska <skorskaga@gmail.com>
2022-10-20 10:23:39 +0100

    Init commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -33,40 +33,6 @@ export default function Products() {
 
   return <>
   <div id={(isBiggerThanMobile && "street-food") || 'false'} className='products'>
-
-    {/* <div className={(isBiggerThanMobile && "nav-arrow") || "hidden"} id="left-arrow" onClick={() => {
-      if (position < 0) {
-         setPosition(position + window.innerWidth / 2)
-        } else {
-          setPosition(0)
-        }
-    }}>&#10094;</div>
-    <div className={(isBiggerThanMobile && "nav-arrow") || "hidden"} id="right-arrow" onClick={() => {
-      setWidthDiff(900 - window.innerWidth) //widthDiff will be  +200
-      if (position === 0) {
-        if (900 - window.innerWidth < window.innerWidth / 2) {
-          setPosition(0 - widthDiff);
-          document.querySelector('.slidable-cards').style.left = `${window.innerWidth - 900}px`;
-          console.log('position should be negative number here: ', position);
-            return     //position will be -400
-        } else {
-          setPosition(position - window.innerWidth / 2)
-        }
-      } else
-      if (Math.abs(position) + window.innerWidth / 2 > widthDiff) {
-        setPosition(0 - widthDiff)     //position will be -400
-      } else {
-        setPosition(position - window.innerWidth / 2)     //position will be -400
-      }
-      
-      // if (Math.abs(position) > widthDiff)  {
-      //   setPosition(0 - widthDiff)
-      // }
-
-       
-        console.log('position after: ', position, 'barlength - windowlength: ', widthDiff)
-      
-    }}>&#10095;</div> */}
     <header><span className='red-letters'>Starters &</span><span className='green-letters'> Appetisers</span></header>
       {/* // style={{"right": `${position}`}} */}
     <div id="moveable-street-food" className={(isBiggerThanMobile && "slidable-cards") ||"cards-class"}> 

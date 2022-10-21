37f99ceb499d09d7e6a7fae76defd87317ceaa11
agiskorska <skorskaga@gmail.com>
2022-10-21 15:01:22 +0100

    Remove console.log

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -12,7 +12,7 @@ const calculateTotal = (array) => {
   for (let item of array) {
     console.log(item.price * item.amount)
     total += parseFloat(item.price)*parseFloat(item.amount)
-    console.log(total.toFixed(2))
+
   }
   return total.toFixed(2)
 
@@ -24,7 +24,7 @@ const reducer = (state = initialStore, action) => {
     console.log('currentProduct: ', currentProduct)
     console.log('action.payload: ', action.payload)
     if (currentProduct.length > 0) {
-      currentProduct[0].price = action.payload.price //TODO why price comes in as undefined the first time?
+      currentProduct[0].price = action.payload.price
       currentProduct[0].amount = action.payload.amount
       console.log(state)
       return {

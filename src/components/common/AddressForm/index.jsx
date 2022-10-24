0f41b5bee7165b4dc6be19253372cd403f99ba09
agiskorska <skorskaga@gmail.com>
2022-10-24 12:22:18 +0100

    Fix bug

--- src/components/common/AddressForm/index.jsx
+++ src/components/common/AddressForm/index.jsx
@@ -1,21 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from './index.module.css'
 
 function AddressForm({addressString}) {
-  const array = addressString.split(',')
+  const array= addressString.split(',')
   const [firstLine, setFirstLine] = useState(array[0])
   const [secondLine, setSecondLine] = useState(array.length > 3 ? array[1] : '')
   const [thirdLine, setThirdLine] = useState(array.length > 4 ? array[1] : '')
   const [postcode, setPostcode] = useState(array[array.length - 1])
   const [town, setTown] = useState(array[array.length - 2])
   const [name, setName] = useState()
-
+  useEffect(() => {
+    const array = addressString.split(',')
+    setFirstLine(array[0])
+    setSecondLine(array.length > 3 ? array[1] : '')
+    setThirdLine(array.length > 4 ? array[1] : '')
+    setPostcode(array[array.length - 1])
+    setTown(array[array.length - 2])
+  }, [addressString])
   return (
     <form className={styles.contentWrapper}>
       <div className={styles.checkoutInfo}>
       <h2>{'Checkout'}</h2>
       <label >Your name</label>
-      <input type="text" className={styles.textInput} value={firstLine} onChange={(e)=>setFirstLine(e.target.value)} placeholder='full name'></input>
+      <input type="text" className={styles.textInput} value={name} onChange={(e)=>setName(e.target.value)} placeholder='full name'></input>
       
       <label >First line of address</label>
       <input type="text" className={styles.textInput} value={firstLine} onChange={(e)=>setFirstLine(e.target.value)} placeholder='first line'></input>

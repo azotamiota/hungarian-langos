cdab86c89763167881042ce007c62d49733cd21d
azotamiota <azotamiota@gmail.com>
2022-11-09 15:38:39 +0000

    Add X to BasketCard

--- src/components/common/BasketCard/index.jsx
+++ src/components/common/BasketCard/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import AmountButtons from '../AmountButtons'
 import styles from './index.module.css'
 
 function BasketCard({index, name, price}) {
 
+  const dispatch = useDispatch()
   const actualProduct = useSelector(state => state.order.products.filter(prod => prod.name === name))
 
 
@@ -12,7 +13,7 @@ function BasketCard({index, name, price}) {
 
     <>{actualProduct[0]['amount'] > 0 &&
       <div className={styles.root}> 
-        <div className={styles.number}>{index + 1} </div>
+        {/* <div className={styles.number}>{index + 1} </div> */}
         <img className={styles.foodImage} src='https://images.unsplash.com/photo-1602881916963-5daf2d97c06e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' alt=''/>
         {/* <div className={styles.description}> */}
           <div className={styles.title}>{name}</div>
@@ -20,7 +21,9 @@ function BasketCard({index, name, price}) {
         <div className={styles.amountChange}>
           <AmountButtons name={name} price={price} amount={actualProduct.length > 0 ? actualProduct[0]['amount'] : 0}/> 
         </div>
-        <span>{actualProduct.length > 0 && '£' + (actualProduct[0]['amount'] * price).toFixed(2)}</span>
+        <div>{actualProduct.length > 0 && '£' + (actualProduct[0]['amount'] * price).toFixed(2)}</div>
+        <div className={styles.closeBasket} onClick={() => console.log('actualProduct: ', actualProduct)}>❌</div>
+
       </div>}
     </>
 
